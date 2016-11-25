0cb02bf05330e40f740577cef00d4922c9c5e9ad
zhong <zhong@zhong-HP>
2016-11-25 23:53:53 +0800

    git pull test

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -2,47 +2,15 @@
 use Test\Test;
 
 /**
-* \HomeController
-*/
+ * \HomeController
+ */
 class HomeController extends BaseController {
-
-  public function home()
-  {
-    // var_dump(Article::count());
-//     $data = ['title'=>'你是谁？?', 'email'=>'1@baiducom'];
-//     $validator = $this->validate($data, [
-//       'title' => 'required|numeric|integer|min:3|max:4',
-//       'email' => 'required|email',
-//     ]);
-//     if ( !$validator->success ) {
-//       foreach ($validator->errors as $error) {
-//         echo $error.'<br>';
-//       }
-//     }
-    
-//     (new Test())->test();
-    var_dump((new \Test\Test())->test());
-    var_dump(\Test\Test::first());
-    Log::debug('First Debug Info.');
-    /*
-    // mail sample
-    Mail::to('foo@bar.io')->from('bar@foo.io')
-                          ->title('Foo Bar')
-                          ->content('<h1>Hello~~</h1>')
-                          ->send();
-    // redis sample
-    Redis::set('key','value',3000,'ms');
-    echo Redis::get('key');
-    */
-
-    // return View
-    return View::make('home')->with('article',Article::first())
-                              ->withTitle('TinyLara :-D')
-                              ->withFooBar('foo_bar');
-
-    // return String
-    return 'Hello TinyLara!';
-
-    // or you can return Nothing.
-  }
+    public function home() {
+        var_dump((new \Test\Test())->test());
+//         var_dump(\Test\Test::first());
+        Log::debug('First Debug Info.');
+//         return View::make('home')->with('article', Article::first())->withTitle('TinyLara :-D')->withFooBar('foo_bar');
+        
+        return 'Hello TinyLara!';
+    }
 }
