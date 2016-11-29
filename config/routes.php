926d10c5bfc499a1096cc9fa18f9daff6d8e1566
zhong <zhong@zhong-HP>
2016-11-29 23:02:27 +0800

    test

--- config/routes.php
+++ config/routes.php
@@ -1,19 +1,19 @@
 <?php
-
 use TinyLara\Routing\Router as Route;
 
 Route::get('/', 'HomeController@home');
 
-Route::any('foo', function() {
-  echo "Foo!";
+Route::any('foo', function () {
+    echo "Foo!";
 });
+Route::get('/test', $callback);
 
-Route::filter(function() {
-  return isset($_GET['token']) && $_GET['token'] == 1;
-}, function(){
-  Route::any('bar', function() {
-    echo "Bar!<br>Filter Success!";
-  });
+Route::filter(function () {
+    return isset($_GET['token']) && $_GET['token'] == 1;
+}, function () {
+    Route::any('bar', function () {
+        echo "Bar!<br>Filter Success!";
+    });
 });
 
 Route::dispatch('View@process');
